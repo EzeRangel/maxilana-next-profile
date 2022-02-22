7ef7ee271b9f3180410f72e2980a6e87a996b05d
Ezequiel Rangel <chekelinho@gmail.com>
2022-02-22 12:43:24 -0700

    Initial page

---
+++ components/ui/Menu/Menu.jsx
@@ -0,0 +1,43 @@
+import Link from 'next/link';
+import { MdOutlineFirstPage, MdOutlineLastPage } from 'react-icons/md';
+
+import useToggle from '../../../hooks/useToggle';
+
+const Menu = () => {
+  const { hidden, updateToggle } = useToggle();
+
+  let className = "Menu";
+
+  if(hidden === true){
+    className += " Menu--hidden";
+  }
+
+  return (
+    <aside className={className}>
+      <header className="Menu__header">
+        <h6 className="Menu__title">Menú</h6>
+        <span className="Menu__toggle" onClick={updateToggle}>
+          {
+            hidden === true
+              ? <MdOutlineLastPage />
+              : <MdOutlineFirstPage />
+          }
+        </span>
+      </header>
+      <ul className="Menu__list">
+        <li className="Menu__item">
+          <Link href="/">
+            Ver tarjetas
+          </Link>
+        </li>
+        <li className="Menu__item">
+          <Link href="/agregar-tarjeta">
+            Agregar tarjeta
+          </Link>
+        </li>
+      </ul>
+    </aside>
+  )
+}
+
+export default Menu;
