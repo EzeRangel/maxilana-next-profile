ccf27532249e2630f96958c483cdebf88a07b3ed
Ezequiel Rangel <chekelinho@gmail.com>
2022-02-25 11:35:21 -0700

    Arregla warning en custom document

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ class MyDocument extends Document {
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
-            crossOrigin={true}
+            crossOrigin="true"
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Bellota+Text:wght@300;400&family=Montserrat:wght@400;700&display=swap"
